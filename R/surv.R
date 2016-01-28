0dbc4ed277022224a46ee697de9ca0a26ff1f9a6
Robert Redd <raredd01@gmail.com>
2016-01-28 15:08:32 -0500

    add survdiff_pairs

--- R/surv.R
+++ R/surv.R
@@ -1,5 +1,6 @@
 ### survival stuff
-# kmplot, kmplot_by, local_coxph_test, surv_cp, surv_summary, surv_table
+# kmplot, kmplot_by, local_coxph_test, surv_cp, surv_summary, surv_table,
+# survdiff_pairs
 ###
 
 
@@ -774,4 +775,60 @@ surv_table <- function(s, digits = 3, times = pretty(range(s$time)), ...) {
                    sprintf('OR (%s%% CI)', s$conf.int * 100)))
   }
   if (is.list(summ)) Map(f = f, summ) else f(summ)
+}
+
+#' Pairwise survival group comparisons
+#' 
+#' Evaluate differences in survival curves for all pairs of a grouping
+#' variable. This function currently works for one \code{factor}-like
+#' variable, and all unqiue levels are treated as a sub-group.
+#' 
+#' @param s a \code{\link[survival]{survdiff}} object
+#' @param ... additional arguments passed to \code{\link{survdiff}} such as
+#' \code{na.action} to filter missing data or \code{rho} to control the type
+#' of test
+#' @param method p-value correction method; see \code{\link{p.adjust}}
+#' @param digits integer indicating the number of decimal places to be used
+#' 
+#' @seealso
+#' \code{\link[rawr]{pvalr}}; \code{\link{survdiff}}; \code{\link{p.adjust}};
+#' \code{\link[rms]{contrast}}
+#' 
+#' @examples
+#' library('survival')
+#' fit1 <- survdiff(Surv(time, status) ~ sex, data = lung)
+#' survdiff_pairs(fit1)
+#' 
+#' ## note that despite a numeric group variable, contrasts
+#' ## are calculated for each unique level combination
+#' dd <- lung[lung$ph.ecog %in% 0:2, ]
+#' fit2 <- survdiff(Surv(time, status) ~ ph.ecog, data = dd)
+#' survdiff_pairs(fit2)
+#' 
+#' ## compare
+#' survdiff(Surv(time, status) ~ ph.ecog, data = dd[dd$ph.ecog %in% 0:1, ])
+#' 
+#' @export
+
+survdiff_pairs <- function(s, ..., method = 'bonferroni',
+                           digits = getOption('digits')) {
+  stopifnot(inherits(s, 'survdiff'))
+  rhs <- all.vars(s$call$formula)[-(1:2)]
+  stopifnot(length(rhs) == 1L)
+  data <- eval(s$call$data, envir = parent.frame())
+  unq <- sort(unique(data[, rhs]))
+  idx <- seq_along(unq)
+  res <- matrix(0, length(unq), length(unq), dimnames = list(unq, unq))
+  names(dimnames(res)) <- c(rhs, rhs)
+  
+  for (ii in idx)
+    for (jj in (idx)[-ii])
+      res[ii, jj] <- survdiff(as.formula(s$call$formula), ...,
+                        data = data[data[, rhs] %in% unq[c(ii, jj)], ])$chisq
+  pvu <- apply(res, 1:2, function(x) pchisq(x, 1, lower.tail = FALSE))
+  pvc <- pvu[lower.tri(pvu)]
+  pvc <- p.adjust(pvc, method = method, n = length(pvc))
+  pvu[upper.tri(pvu)] <- pvc
+  
+  lapply(list(chi.sq = res, p.value = pvu), round, digits = digits)
 }
