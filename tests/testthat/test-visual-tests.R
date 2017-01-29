228afe0b52e073e4272d55f44d3f95f845cb7709
Robert Redd <raredd01@gmail.com>
2017-01-29 17:13:53 -0500

    update visual tests

--- tests/testthat/test-visual-tests.R
+++ tests/testthat/test-visual-tests.R
@@ -5,6 +5,19 @@ test_that('Visual tests for kmplot and kmplot_by', {
     ## visual tests
     library('rawr')
     library('survival')
+    
+    
+    ## identical plots (minus some aesthetics)
+    s <- survfit(Surv(time, status) ~ sex, lung)
+    mar <- c(5,6,2,2)
+    par(mar = mar)
+    plot(s, mark.time = TRUE, col = 1:2)
+    kmplot(s, atrisk = FALSE, mar = mar)
+    kmplot_by(s, atrisk = FALSE, mar = mar)
+    kmplot_by(time = 'time', strata = 'sex', event = 'status', data = lung,
+              atrisk = FALSE, mar = mar)
+    
+    
     colon2 <- within(colon[duplicated(colon$id), ], {
       pfs_time <- time
       pfs_ind  <- status
@@ -15,35 +28,37 @@ test_that('Visual tests for kmplot and kmplot_by', {
     ## --- color mapping --- ##
     
     ## colors should map
-    kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE)
+    kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE, map.col = TRUE)
     
     ## colors should _not_ map
-    kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE, col.surv = 1)
+    kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE)
     
     ## colors should map
     kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE,
-              col.surv = c(Male = 'green', Female = 'blue'))
+              col.surv = c(Male = 'green', Other = 'blue', Female = 'red'))
     
     ## --- labels are not mapped -- low priority --- ##
     
     kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE, strata_lab = 1:2,
               col.surv = c('1' = 'green', '2' = 'blue'))
     
-    kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE, strata_lab = c('1','2'))
+    kmplot_by('sex', 'pfs', colon2, by = 'sex', single = FALSE,
+              strata_lab = c('1','2'))
     
     ## labels are mapped
-    kmplot_by('sex', 'pfs', colon2, by = 'rx', single = FALSE, strata_lab = c('1','2'),
-              col.surv = c('1' = 'magenta', '2' = 'blue'))
+    kmplot_by('sex', 'pfs', colon2, by = 'rx', single = FALSE,
+              strata_lab = c('abc','def'),
+              col.surv = c('abc' = 'magenta', 'def' = 'blue'))
     
     ## --- drop a level for one plot --- ##
     
     colon2$rx[colon2$rx == 'Obs' & colon2$sex == 'Male'] <- NA
     s <- survfit(Surv(pfs_time, pfs_ind) ~ rx, colon2)
     
+    ## base plot
     kmplot(s, col.surv = 1:3)
-    
-    kmplot(s, col.surv = c('rx=Obs' = 'red')) ## expect warning
-    
+    ## expect warning
+    kmplot(s, col.surv = c('rx=Obs' = 'red'))
     ## second plot should drop black line not green
     kmplot_by(s, by = 'sex', col.surv = 1:3)
     
