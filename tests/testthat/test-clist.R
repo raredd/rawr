bf8ba81067a6f8e55ec97c497b43bad1ecedfb1a
Robert Redd <raredd01@gmail.com>
2017-03-19 14:13:43 -0400

    clist - add rbindx and cbindx options, add tests

--- tests/testthat/test-clist.R
+++ tests/testthat/test-clist.R
@@ -7,8 +7,28 @@ test_that('clist expects uniquely-named lists', {
 })
 
 test_that('clist combines list elements by class method', {
+  
   bp1 <- boxplot(mpg ~ vs, data = mtcars[mtcars$vs == 0, ], plot = FALSE)
   bp2 <- boxplot(mpg ~ vs, data = mtcars[mtcars$vs == 1, ], plot = FALSE)
   bp  <- boxplot(mpg ~ vs, data = mtcars, plot = FALSE)
+  
   expect_identical(clist(bp1, bp2), bp)
+  
+})
+
+test_that('rbind* and cbind* methods give identical results', {
+  
+  bp1 <- boxplot(mpg ~ vs, data = mtcars[mtcars$vs == 0, ], plot = FALSE)
+  bp2 <- boxplot(mpg ~ vs, data = mtcars[mtcars$vs == 1, ], plot = FALSE)
+  
+  expect_identical(
+    clist(bp1, bp2, how = 'rbind'),
+    clist(bp1, bp2, how = 'rbindx')
+  )
+  
+  expect_identical(
+    clist(bp1, bp2, how = 'cbind'),
+    clist(bp1, bp2, how = 'cbindx')
+  )
+  
 })
