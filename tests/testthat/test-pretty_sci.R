af29659458473cffe4767e434a8bc2fa40d02fb1
Robert Redd <raredd01@gmail.com>
2018-12-03 00:59:19 -0500

    move misc/unused plotting fns to plotr

--- tests/testthat/test-pretty_sci.R
+++
@@ -1,8 +0,0 @@
-context('misc tests')
-
-test_that('oom returns correct orders of magnitude for different types', {
-  x <- c(1, 10, 1000, 10000, 1e100)
-  expect_identical(oom(-x), abs(oom(1 / x)))
-  expect_true(rawr::all_equal2(5, oom(1e5), oom(-1e5)))
-  expect_true(rawr::all_equal2(-5, oom(1e-5), oom(-1e-5)))
-})
