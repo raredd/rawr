25948127eef7d79484dff50c9d574ca72d0c59cb
Robert Redd <raredd01@gmail.com>
2022-01-25 19:14:03 -0500

    pvalr - major update, use signif2

--- R/utils2.R
+++ R/utils2.R
@@ -773,16 +773,21 @@ intr2 <- function(..., fun = median, na.rm = FALSE, digits = 0L, conf.int = 0.95
 }
 
 #' p-value formatter
-#'
+#' 
+#' @description
 #' Formats several cases of p-values; see details.
-#'
-#' \code{pvalr} will deal with several cases of common p-values: 1) p-values
-#' which are > 0.1 will be rounded to two decimal places (and keep any trailing
-#' 0s) since we are not usually interested in precision of insignificant
-#' values; 2) p-values which are less than the specified \code{sig.level} will
-#' be formatted as \code{< 0.001}, for example; 3) p-values that are less than
-#' 0.01 but greater than \code{sig.level} will be precise to \code{digits} and
-#' keep any trailing 0s.
+#' 
+#' Note that a previous \code{pvalr} function had additional arguments. For
+#' this version, see \code{rawr2::pvalr}.
+#' 
+#' @details
+#' \code{pvalr} rounds numeric values to a specified number of significant
+#' \code{digits} and add (if necessary) trailing 0s. The result will be a
+#' character string to prevent loss of significant digits.
+#' 
+#' Note that negative values are treated as small p-values (similar to
+#' \code{format.pval}), but unlike \code{format.pval}, values greater than 1
+#' are formatted as \code{> 1 - 1 / 10 ^ digits}
 #'
 #' \code{pvalr2} deals with common cases of character string p-values which are
 #' not ideal (0.000, 1.00, etc.) and will leave others unchanged.
@@ -791,70 +796,62 @@ intr2 <- function(..., fun = median, na.rm = FALSE, digits = 0L, conf.int = 0.95
 #'   \code{pvalr2}, a vector of p-values as character strings
 #' @param sig.limit lower bound for precision; smaller values will be shown as
 #'   \code{< sig.limit}
-#' @param digits number of digits past the decimal point to keep
+#' @param digits number of significant digits
+#' @param scientific see \code{\link[base]{format}}
 #' @param html logical; if \code{TRUE}, uses HTML entities for \code{<}
 #'   and \code{>}
 #' @param show.p logical; if \code{TRUE}, inserts \code{p = }, \code{p < }, or
 #'   \code{p > } where appropriate
-#' @param journal logical; if \code{TRUE}, p-values greater than
-#'   \code{sig.limit} are rounded to \code{digits}
-#' @param limits a vector controlling the number of digits used in p-values
-#' 
-#'   if \code{journal = TRUE}, \code{limits} should be length one, and p-values
-#'   will be rounded to two digits for values greater than \code{limits} or
-#'   \code{digits} for values between \code{sig.limit} and \code{limits}
-#' 
-#'   if \code{journal = FALSE}, \code{limits} should be length two, and
-#'   p-values will be rounded to one digit for values greater than
-#'   \code{limits[2L]}, two digits for values between \code{limits[1L]} and
-#'   \code{limits[2L]}, and \code{digits} for values between \code{sig.limit}
-#'   and \code{limits[1L]}
-#' @param ... additional arguments passed to \code{\link{format.pval}} or
-#'   further to \code{\link{format}}
+#' @param ... ignored
 #'
 #' @seealso
-#' \code{\link[rawr]{roundr}}; \code{\link[base]{format.pval}}
+#' \code{\link[rawr]{roundr}}; \code{\link[base]{format.pval}};
+#' \code{rawr:::signif2}
 #'
 #' @examples
-#' pv <- c(-1, 0.00001, 0.0042, 0.0601, 0.1335, 0.4999, 0.51, 0.89, 0.9, 1)
+#' pv <- c(-1, 0.001, 0.0001, 0.0042, 0.0601, 0.1335, 0.4999, 0.9, 0.997, 1, 2)
 #' format.pval(pv, eps = 0.001)
 #'
 #' pvalr(pv)
-#' pvalr(pv, journal = FALSE)
-#' pvalr(pv, limits = 0.1)
+#' pvalr(pv, digits = 3)
 #' pvalr(pv, show.p = TRUE, html = TRUE)
+#' 
+#' \dontrun{
+#' ## older version of pvalr in rawr2 package
+#' ## note differences in digits used
+#' rawr2::pvalr(pv)
+#' rawr2::pvalr(pv, journal = FALSE)
+#' rawr2::pvalr(pv, limits = 0.1)
+#' }
 #'
 #' @export
 
-pvalr <- function(pv, sig.limit = 10 ^ -digits, digits = 3L, html = FALSE,
-                  show.p = FALSE, journal = TRUE, limits = c(0.01, 0.5), ...) {
+pvalr <- function(pv, sig.limit = 0.001, digits = 2L, scientific = FALSE,
+                  html = FALSE, show.p = FALSE, ...) {
   stopifnot(
     sig.limit > 0,
-    sig.limit < 1,
-    (journal & length(limits) > 0L) |
-      (!journal & length(limits) > 1L)
+    sig.limit < 1
   )
-
-  show.p <- show.p + 1L
-  html <- html + 1L
-  limits <- sort(limits)
-
+  
+  pstr <- c('', 'p ')[show.p + 1L]
+  high <- 1 - 1 / 10 ^ digits
+  
   sapply(pv, function(x) {
     if (is.na(x) | !nzchar(x))
-      return(NA)
-    if (x >= 0.99)
-      return(paste0(c('', 'p ')[show.p], c('> ', '&gt; ')[html], '0.99'))
-    if (x >= 0.9 && !journal)
-      return(paste0(c('', 'p ')[show.p], c('> ', '&gt; ')[html], '0.9'))
-    if (x < sig.limit) {
-      paste0(c('', 'p ')[show.p], c('< ', '&lt; ')[html],
-             format.pval(sig.limit, ...))
-    } else {
-      nd <- if (journal)
-        c(digits, 2L)[findInterval(x, c(-Inf, limits[1L], Inf))]
-      else c(digits, 2L, 1L)[findInterval(x, c(-Inf, limits[1:2], Inf))]
-      paste0(c('', 'p = ')[show.p], roundr(x, nd))
-    }
+      NA
+    else if (x > high)
+      paste0(pstr, c('> ', '&gt; ')[html + 1L], high)
+    else if (x < sig.limit)
+      paste0(pstr, c('< ', '&lt; ')[html + 1L],
+             format.pval(sig.limit, scientific = scientific))
+    else paste0(c('', 'p = ')[show.p + 1L], signif2(x, digits))
+  })
+}
+
+signif2 <- function(x, digits = 6L) {
+  sapply(x, function(xx) {
+    s <- signif(xx, digits = digits)
+    formatC(s, digits = digits, format = 'fg', flag = '#')
   })
 }
 
